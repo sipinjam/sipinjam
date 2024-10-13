805eb505c183d8d2d39d15758b7d0bc3ed4a828a
DanuAlamsyah <danualamsyah070@gmail.com>
2024-10-14 05:22:04 +0700

    faq

---
+++ lib/pages/faq.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+// Define the FAQ model (struktur data untuk FAQ)
+class Faq {
+  final String question;
+  final String answer;
+
+  Faq({required this.question, required this.answer});
+}
+
+class FaqScreen extends StatelessWidget {
+  // List of FAQ items
+  final List<Faq> faqs = [
+    Faq(
+      question: 'Bagaimana Cara Pengajuan Peminjaman Ruangan?',
+      answer:
+          '1. Membuat surat peminjaman tempat. 2. Meminta disposisi helper. 3. Meminta memo ke BEM. 4. Surat diserahkan ke Bapak Unggul.',
+    ),
+    Faq(
+      question: 'Apakah ada batasan waktu dalam peminjaman tempat?',
+      answer: 'Batasan waktu tergantung ketersediaan ruangan.',
+    ),
+    Faq(
+      question: 'Apakah saya bisa memodifikasi peminjaman setelah dikonfirmasi?',
+      answer: 'Anda harus menghubungi admin untuk perubahan peminjaman.',
+    ),
+    Faq(
+      question: 'Apakah ada biaya tambahan selain biaya sewa tempat?',
+      answer: 'Tidak ada biaya tambahan.',
+    ),
+    // Tambah lebih banyak FAQ jika perlu
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FAQ'),
+      ),
+      body: ListView.builder(
+        itemCount: faqs.length,
+        itemBuilder: (context, index) {
+          return ExpansionTile(
+            title: Text(faqs[index].question),
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(faqs[index].answer),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
