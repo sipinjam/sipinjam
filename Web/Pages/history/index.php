d3cb09903aecc124a03f28ee0686ed8ee4975c0a
Nicholas Ernesto <nicholas.ernesto23@gmail.com>
2024-10-18 22:28:13 +0700

    rename history

--- Web/Pages/history/index.php
+++ Web/Pages/history/index.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SIPINJAM - History</title>
-    <link rel="stylesheet" href="history.css">
 </head>
 
 <body>
