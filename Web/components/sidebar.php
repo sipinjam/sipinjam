62d9181bfefeb86f6d7774320f38a8a5e710f88a
Nicholas Ernesto <nicholas.ernesto23@gmail.com>
2024-10-25 14:42:51 +0700

    sidebar lagi

--- Web/components/sidebar.php
+++ Web/components/sidebar.php
@@ -70,9 +70,8 @@ $current_page = basename($_SERVER['REQUEST_URI']);
                 <a href=""
                     class="flex items-center p-2 text-gray-400 rounded-lg hover:bg-gray-700 group hover:text-white">
                     <svg class="flex-shrink-0 w-5 h-5 text-gray-400 transition duration-75 group-hover:text-white transition duration-75"
-                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                        <path
-                            d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 5 5">
+
                     </svg>
                     <span class="flex-1 ms-3 whitespace-nowrap">Logout</span>
                 </a>
@@ -80,3 +79,19 @@ $current_page = basename($_SERVER['REQUEST_URI']);
         </ul>
     </div>
 </aside>
+
+<script>
+if (!localStorage.getItem('loggedIn')) {
+    alert("Anda harus login terlebih dahulu.");
+    window.location.href = '/sipinjamfix/sipinjam/web/'; // Arahkan kembali ke halaman login jika belum login
+}
+
+function logout() {
+    // Hapus data di localStorage
+    localStorage.removeItem('nama_peminjam');
+    localStorage.removeItem('loggedIn');
+
+    // Alihkan pengguna ke halaman login
+    window.location.href = '/sipinjamfix/sipinjam/web/pages/login';
+}
+</script>
