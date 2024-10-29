6c0fe485a7e21850e9cec3711e490850ee9ed002
Rohimatun <rohimatunnurin13@gmail.com>
2024-10-29 11:48:42 +0700

    header

--- Web/Pages/home/index.php
+++ Web/Pages/home/index.php
@@ -16,16 +16,11 @@
     <!-- End Sidebar -->
 
     <!-- Header -->
-    <div class="w-full h-[120px] bg-gradient-to-br from-blue-800 to-blue-300 p-4 shadow-md fixed top-0 left-0 z-20">
-        <div class="flex items-center justify-between max-w-7xl mx-auto">
-            <div class="flex items-center space-x-3">
-                <img src="../../Sources/Img/LogoPolines.png" alt="Logo" class="h-20 w-20">
-                <h1 class="text-white text-3xl font-bold pl-3">SIPINJAM</h1>
-            </div>
-        </div>
-    </div>
+    <?php include '../../components/header.php' ?>
+    <!-- End Header -->
 
-    
+
+    <!-- Main Menu -->
     <div class="flex-grow pt-[120px]">
 
     <!--SEARCH -->
