a91e3341ab85c63355eb89591ee27236d630940f
DanuAlamsyah <danualamsyah070@gmail.com>
2024-10-29 19:32:19 +0700

    button di kotak masuk

--- Web/Pages/kotakMasuk/index.php
+++ Web/Pages/kotakMasuk/index.php
@@ -16,12 +16,12 @@
 
     <!-- Main Content -->
     <div class="p-4 sm:ml-64">
-        <h1 class="text-4xl font-bold p-4">Riwayat Aktivitas</h1>
+        <h1 class="text-4xl font-bold p-4">Kotak Masuk</h1>
         <!-- Responsive Grid -->
         <div class="pt-5 grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4 mb-4">
             <a href="#"
                 class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100">
-                <img class="object-cover w-full rounded-t-lg h-48 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                <img class="object-cover w-full rounded-t-lg h-48 md:h-full md:w-48 md:rounded-none md:rounded-s-lg"
                     src="../../Sources/Img/gedungkuliah-terpadu.png" alt="png">
                 <div class="flex flex-col justify-between p-4 leading-normal">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Gedung Kuliah Terpadu</h5>
@@ -30,12 +30,13 @@
                     <p class="text-sm text-gray-600 pt-1">Tanggal Pinjam:</p>
                     <p class="text-sm font-bold">28 September 2024</p>
                     <p class="text-sm mt-2">Status: <span class="text-yellow-600 font-bold">Proses</span></p>
+                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Lihat Detail Peminjaman</button>
                 </div>
             </a>
 
             <a href="#"
                 class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100">
-                <img class="object-cover w-full rounded-t-lg h-48 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                <img class="object-cover w-full rounded-t-lg h-48 md:h-full md:w-48 md:rounded-none md:rounded-s-lg"
                     src="../../Sources/Img/gedungkuliah-terpadu.png" alt="png">
                 <div class="flex flex-col justify-between p-4 leading-normal">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Gedung Kuliah Terpadu</h5>
@@ -44,12 +45,13 @@
                     <p class="text-sm text-gray-600 pt-1">Tanggal Pinjam:</p>
                     <p class="text-sm font-bold">28 September 2024</p>
                     <p class="text-sm mt-2">Status: <span class="text-yellow-600 font-bold">Proses</span></p>
+                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Lihat Detail Peminjaman</button>
                 </div>
             </a>
 
             <a href="#"
                 class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100">
-                <img class="object-cover w-full rounded-t-lg h-48 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                <img class="object-cover w-full rounded-t-lg h-48 md:h-full md:w-48 md:rounded-none md:rounded-s-lg"
                     src="../../Sources/Img/gedungkuliah-terpadu.png" alt="png">
                 <div class="flex flex-col justify-between p-4 leading-normal">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Gedung Kuliah Terpadu</h5>
@@ -58,12 +60,13 @@
                     <p class="text-sm text-gray-600 pt-1">Tanggal Pinjam:</p>
                     <p class="text-sm font-bold">28 September 2024</p>
                     <p class="text-sm mt-2">Status: <span class="text-yellow-600 font-bold">Proses</span></p>
+                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Lihat Detail Peminjaman</button>
                 </div>
             </a>
 
             <a href="#"
                 class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100">
-                <img class="object-cover w-full rounded-t-lg h-48 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                <img class="object-cover w-full rounded-t-lg h-48 md:h-full md:w-48 md:rounded-none md:rounded-s-lg"
                     src="../../Sources/Img/gedungkuliah-terpadu.png" alt="png">
                 <div class="flex flex-col justify-between p-4 leading-normal">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Gedung Kuliah Terpadu</h5>
@@ -72,16 +75,12 @@
                     <p class="text-sm text-gray-600 pt-1">Tanggal Pinjam:</p>
                     <p class="text-sm font-bold">28 September 2024</p>
                     <p class="text-sm mt-2">Status: <span class="text-yellow-600 font-bold">Proses</span></p>
+                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Lihat Detail Peminjaman</button>
                 </div>
             </a>
         </div>   
     </div>
     <!-- End Main -->
-    <div class="mt-4 text-left fixed left-0 bottom-9 p-4 pl-72">
-        <a href="#" class="text-blue-500 hover:underline">Lihat Daftar Panitia</a>
-        <br>
-        <a href="#" class="text-blue-500 hover:underline">Lihat Surat Peminjaman</a>
-    </div>
 </body>
 
 </html>
