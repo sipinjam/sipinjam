0eb201726a8802c2ede6c4e10bad4d2e0e2cfbfa
DanuAlamsyah <danualamsyah070@gmail.com>
2024-10-29 15:29:05 +0700

    buat kotakmasuk

---
+++ Web/Pages/kotakMasuk/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="../../Public/theme.css">
+    <title>SIPINJAM - Riwayat</title>
+</head>
+
+<body>
+    <!-- Sidebar -->
+    <?php include '../../Components/sidebar.php'; ?>
+    <!-- End Sidebar -->
+
+    <!-- Main Content -->
+    <div class="p-4 sm:ml-64">
+        <h1 class="text-4xl font-bold p-4">Riwayat Aktivitas</h1>
+        <!-- Responsive Grid -->
+        <div class="pt-5 grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4 mb-4">
+            <a href="#"
+                class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100">
+                <img class="object-cover w-full rounded-t-lg h-48 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                    src="../../Sources/Img/gedungkuliah-terpadu.png" alt="png">
+                <div class="flex flex-col justify-between p-4 leading-normal">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Gedung Kuliah Terpadu</h5>
+                    <p class="text-sm text-gray-600 pt-2">Peminjam:</p>
+                    <p class="text-sm font-bold">Rohkris</p>
+                    <p class="text-sm text-gray-600 pt-1">Tanggal Pinjam:</p>
+                    <p class="text-sm font-bold">28 September 2024</p>
+                    <p class="text-sm mt-2">Status: <span class="text-yellow-600 font-bold">Proses</span></p>
+                </div>
+            </a>
+
+            <a href="#"
+                class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100">
+                <img class="object-cover w-full rounded-t-lg h-48 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                    src="../../Sources/Img/gedungkuliah-terpadu.png" alt="png">
+                <div class="flex flex-col justify-between p-4 leading-normal">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Gedung Kuliah Terpadu</h5>
+                    <p class="text-sm text-gray-600 pt-2">Peminjam:</p>
+                    <p class="text-sm font-bold">Rohkris</p>
+                    <p class="text-sm text-gray-600 pt-1">Tanggal Pinjam:</p>
+                    <p class="text-sm font-bold">28 September 2024</p>
+                    <p class="text-sm mt-2">Status: <span class="text-yellow-600 font-bold">Proses</span></p>
+                </div>
+            </a>
+
+            <a href="#"
+                class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100">
+                <img class="object-cover w-full rounded-t-lg h-48 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                    src="../../Sources/Img/gedungkuliah-terpadu.png" alt="png">
+                <div class="flex flex-col justify-between p-4 leading-normal">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Gedung Kuliah Terpadu</h5>
+                    <p class="text-sm text-gray-600 pt-2">Peminjam:</p>
+                    <p class="text-sm font-bold">Rohkris</p>
+                    <p class="text-sm text-gray-600 pt-1">Tanggal Pinjam:</p>
+                    <p class="text-sm font-bold">28 September 2024</p>
+                    <p class="text-sm mt-2">Status: <span class="text-yellow-600 font-bold">Proses</span></p>
+                </div>
+            </a>
+
+            <a href="#"
+                class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100">
+                <img class="object-cover w-full rounded-t-lg h-48 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                    src="../../Sources/Img/gedungkuliah-terpadu.png" alt="png">
+                <div class="flex flex-col justify-between p-4 leading-normal">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Gedung Kuliah Terpadu</h5>
+                    <p class="text-sm text-gray-600 pt-2">Peminjam:</p>
+                    <p class="text-sm font-bold">Rohkris</p>
+                    <p class="text-sm text-gray-600 pt-1">Tanggal Pinjam:</p>
+                    <p class="text-sm font-bold">28 September 2024</p>
+                    <p class="text-sm mt-2">Status: <span class="text-yellow-600 font-bold">Proses</span></p>
+                </div>
+            </a>
+
+        </div>
+    </div>
+    <!-- End Main -->
+    <div class="mt-4 text-left fixed left-0 bottom-9 p-4 pl-72">
+    <a href="#" class="text-blue-500 hover:underline">Lihat Daftar Panitia</a>
+    <br>
+    <a href="#" class="text-blue-500 hover:underline">Lihat Surat Peminjaman</a>
+</div>
+
+</body>
+
+</html>
