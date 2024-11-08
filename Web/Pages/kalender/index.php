3e44eb2712fcbdf34db716d3e070508732b50db6
DanuAlamsyah <danualamsyah070@gmail.com>
2024-11-08 13:36:43 +0700

    kuning

--- Web/Pages/kalender/index.php
+++ Web/Pages/kalender/index.php
@@ -66,7 +66,7 @@
                 <h2 class="text-lg font-bold mb-2">Keterangan</h2>
                 <div class="space-y-2">
                     <div class="bg-red-500 text-white px-4 py-2 rounded-md">Sesi sudah penuh</div>
-                    <div class="bg-yellow-500 text-white px-4 py-2 rounded-md">Sesi 1</div>
+                    <div class="bg-yellow-400 text-white px-4 py-2 rounded-md">Sesi 1</div>
                     <div class="bg-green-400 text-white px-4 py-2 rounded-md">Sesi 2</div>
                 </div>
             </div>
